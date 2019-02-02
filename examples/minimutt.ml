b921156f649232c867f0098505ffd2e066ef7e2c
Przemek Kaminski <cgenie@gmail.com>
2019-02-02 16:46:50 +0100

    minimutt: fixes to the incorred uid variable

--- examples/minimutt.ml
+++ examples/minimutt.ml
@@ -16,7 +16,7 @@ let sync server ?port username password mailbox =
       let filename = Int32.to_string (uid :> int32) in
       Lwt_unix.openfile filename [O_WRONLY; O_CREAT; O_TRUNC] 0o600 >>= fun fd ->
       let oc = Lwt_io.of_fd ~mode:Lwt_io.Output fd in
-      Lwt_stream.to_list Imap.(uid_fetch imap [uid] Fetch.[Request.body_section ()]) >>= function
+      Lwt_stream.to_list Imap.uid_fetch imap [uid] Imap.Fetch.[Request.body_section ()] >>= function
       | [_, {Imap.Fetch.Response.body_section = [([], None), Some s]; _}] ->
           Lwt_io.write oc s >>= fun () -> Lwt_io.close oc >>= fun () ->
           Lwt_io.eprintlf "OK writing #%s" filename
